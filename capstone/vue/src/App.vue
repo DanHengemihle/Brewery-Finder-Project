cacde118b3bc9ff5b2f8cd69322ee5314fd3b7a2
Dan Hengemihle <danhengemihle@gmail.com>
2022-09-26 19:10:06 -0400

    someone help

--- capstone/vue/src/App.vue
+++ capstone/vue/src/App.vue
@@ -3,15 +3,8 @@
   
   <div id="app">
     <div id="nav">
-<<<<<<< HEAD
-
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB"
-        crossorigin="anonymous">
-
-=======
       <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB"
         crossorigin="anonymous">
->>>>>>> main
       <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
       <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
     </div>
@@ -22,21 +15,9 @@
 
 </template>
 
-<<<<<<< HEAD
-
-<style scoped>
-
-#nav {
-  background-color: #F1F1F1;
-  text-align: center;
-  padding: 40px;
-}
-
-=======
 <script>
 </script>
 
 <style>
->>>>>>> main
 
 </style>
