a03196c55ecdf5c30801330ae258af63dd027b28
Chris Ward <15wardc@gmail.com>
2022-10-03 09:10:07 -0400

    struggling with search api

--- capstone/java/src/main/java/com/techelevator/services/BreweryService.java
+++ capstone/java/src/main/java/com/techelevator/services/BreweryService.java
@@ -1,11 +1,12 @@
 package com.techelevator.services;
 
 import com.techelevator.model.Brewery;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.io.IOException;
 import java.util.List;
 
 public interface BreweryService {
 
-    List<String> getAllBreweries() throws IOException, InterruptedException;
+    List<String> searchBreweries(@RequestParam String search) throws IOException, InterruptedException;
 }
