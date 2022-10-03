83c93771d2b810b3bf0cd6c1c0be87badae5447a
Chris Ward <15wardc@gmail.com>
2022-10-03 15:42:05 -0400

    fixing variable names in brewerydao

--- capstone/java/src/main/java/com/techelevator/controller/BeerController.java
+++ capstone/java/src/main/java/com/techelevator/controller/BeerController.java
@@ -1,44 +1,44 @@
-//package com.techelevator.controller;
-//
-//
-//import com.techelevator.dao.BeerDAO;
-//import com.techelevator.model.Beer;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.*;
-//
-//import javax.validation.Valid;
-//import java.util.List;
-//
-//@RequestMapping("/")
-//@RestController
-//@CrossOrigin
-//public class BeerController {
-//    @Autowired
-//    private BeerDAO beerDAO;
-//
-//    @PostMapping("/beers")
-//    public void create(@RequestBody @Valid Beer beer) {
-//        beerDAO.addNewBeer(beer);
-//    }
-//
-//    @GetMapping("/beers")
-//    public List<Beer> getAllBeers() {
-//        return beerDAO.getAllBeers();
-//    }
-//
-//    @GetMapping("/beers/{id}")
-//    public Beer getBeerById(@PathVariable int id) {
-//        return beerDAO.getBeerById(id);
-//    }
-//
-//    @GetMapping("/beers/{name}")
-//    public int getBeerIdByName(@PathVariable String name) {
-//        return beerDAO.getBeerIdByName(name);
-//    }
-//
-//    @GetMapping("/breweries/{breweryId}")
-//    public List<Beer> getBreweriesById(@PathVariable int id) {
-//        return beerDAO.getBeersByBreweryId(id);
-//    }
-//
-//}
+package com.techelevator.controller;
+
+
+import com.techelevator.dao.BeerDAO;
+import com.techelevator.model.Beer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+
+@RestController
+@CrossOrigin
+public class BeerController {
+    @Autowired
+    private BeerDAO beerDAO;
+
+    @PostMapping("/beers")
+    public void create(@RequestBody @Valid Beer beer) {
+        beerDAO.addNewBeer(beer);
+    }
+
+    @GetMapping("/beers")
+    public List<Beer> getAllBeers() {
+        return beerDAO.getAllBeers();
+    }
+
+    @GetMapping("/beers/{id}")
+    public Beer getBeerById(@PathVariable int id) {
+        return beerDAO.getBeerById(id);
+    }
+
+    @GetMapping("/beers/{name}")
+    public int getBeerIdByName(@PathVariable String name) {
+        return beerDAO.getBeerIdByName(name);
+    }
+
+    @GetMapping("/breweries/{breweryId}")
+    public List<Beer> getBreweriesById(@PathVariable int id) {
+        return beerDAO.getBeersByBreweryId(id);
+    }
+
+}
