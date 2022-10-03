37c3c48bf3ff6698a1d26d17bd8a06330ef7e82f
Niel Kakkad <niel.kakkad@gmail.com>
2022-10-03 09:21:25 -0400

    Not sure if there's anything to push

---
+++ capstone/java/src/main/java/com/techelevator/controller/ReviewController.java
@@ -0,0 +1,27 @@
+//package com.techelevator.controller;
+//
+//
+//import com.techelevator.dao.BeerDAO;
+//import com.techelevator.model.Review;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.access.prepost.PreAuthorize;
+//import org.springframework.web.bind.annotation.*;
+//
+//import java.util.List;
+//
+//@RequestMapping("/")
+//@RestController
+//public class ReviewController {
+//
+//    @Autowired
+//    private BeerDAO beerDAO;
+//
+//    @PreAuthorize("hasAnyRole('ROLE_USER')")
+//    @PostMapping("/beer/reviews")
+//    public void addReview(@RequestBody Review review) {
+//        beerDAO.addReview(review);
+//    }
+//
+////    @GetMapping("/beer/{id}/reviews")
+////    public List<Review> getReviews
+//}
